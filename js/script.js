8fd2cbe7f63e629324b5c37a9058bdb922796283
FaridAxmedov <faridnah@code.edu.az>
2020-05-06 03:06:35 +0400

    sonuncu deyishiklik

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+//x+=100; //x=x+10
+// x-=5;
+// // x/=4
+// x%=6
+
+// var x=10;
+// var y=20;
+
+// // console.log(x/y)
+
+//  var age=3;
+// var result=(age>18) ? "Xosh":"bas";
+
+// console.log(result);
+// //  if (age>18)
+// //  {   console.log("Girin");
+
+//      if(age>30)
+//     console.log("ne gundesen")
+  
+//  } 
+//  else if(age==18)
+//  {
+//      console.log("davaaay");
+//  }
+//  else
+//  {
+//      console.log("BAs bayira");
+//      }
+
+console.log(!((22>19) && (22<23) || (5>4)));
+
